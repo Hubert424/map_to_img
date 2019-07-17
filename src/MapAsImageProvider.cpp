5a30d598d7cf4c37274f69467b6574fabcfe0e5a
Łukasz Mitka <lukasz.mitka@husarion.com>
2019-07-17 14:46:40 +0200

    Refactoring, move class to separate file

---
+++ src/MapAsImageProvider.cpp
@@ -0,0 +1,163 @@
+#include "MapAsImageProvider.h"
+
+MapAsImageProvider::MapAsImageProvider(ros::NodeHandle nh, uint16_t tile_width, uint16_t tile_height)
+{
+    node_handle = nh;
+
+    image_transport_ = new image_transport::ImageTransport(node_handle);
+    image_transport_publisher_full_ = image_transport_->advertise("/map_image/full", 1);
+    image_transport_publisher_tile_ = image_transport_->advertise("/map_image/tile", 1);
+
+    pose_sub_ = node_handle.subscribe("/pose", 1, &MapAsImageProvider::poseUpdate, this);
+    map_sub_ = node_handle.subscribe("/map", 1, &MapAsImageProvider::mapUpdate, this);
+
+    cv_img_full_.header.frame_id = "map_image";
+    cv_img_full_.encoding = sensor_msgs::image_encodings::MONO8;
+    cv_img_full_.image = cv::Mat(INITIAL_MAP_SIZE_X, INITIAL_MAP_SIZE_Y, CV_8U, 127);
+    lastMapUpdate = ros::Time::now();
+    fullMapDelay = ros::Duration(INITIAL_FULL_MAP_DELAY);
+    lastTileUpdate = ros::Time::now();
+    mapTileDelay = ros::Duration(INITIAL_TILE_DELAY);
+
+    cv_img_tile_.header.frame_id = "map_image";
+    cv_img_tile_.encoding = sensor_msgs::image_encodings::MONO8;
+    cv_img_tile_.image = cv::Mat(tile_width, tile_height, CV_8U, 127);
+
+    map_scale = DEFAULT_MAP_SCALE;
+
+    ROS_INFO("Map to Image node started.");
+}
+
+MapAsImageProvider::~MapAsImageProvider()
+{
+    delete image_transport_;
+}
+
+void MapAsImageProvider::setScale(float scale)
+{
+    ROS_INFO("New scale is %f", scale);
+    map_scale = scale;
+}
+
+void MapAsImageProvider::publishFullMap(bool force)
+{
+    if (force)
+    {
+        image_transport_publisher_full_.publish(cv_img_full_.toImageMsg());
+        lastMapUpdate = ros::Time::now();
+    }
+    else if (lastMapUpdate + fullMapDelay < ros::Time::now())
+    {
+        image_transport_publisher_full_.publish(cv_img_full_.toImageMsg());
+        lastMapUpdate = ros::Time::now();
+    }
+}
+
+void MapAsImageProvider::publishMapTile(bool force)
+{
+    if (force)
+    {
+        image_transport_publisher_tile_.publish(cv_img_tile_.toImageMsg());
+        lastTileUpdate = ros::Time::now();
+    }
+    else if (lastTileUpdate + mapTileDelay < ros::Time::now())
+    {
+        image_transport_publisher_tile_.publish(cv_img_tile_.toImageMsg());
+        lastTileUpdate = ros::Time::now();
+    }
+}
+
+void MapAsImageProvider::poseUpdate(const geometry_msgs::PoseStampedConstPtr &pose)
+{
+    pose_ptr_ = pose;
+    tileUpdate();
+}
+
+void MapAsImageProvider::mapUpdate(const nav_msgs::OccupancyGridConstPtr &map)
+{
+    if ((map->info.width < 3) || (map->info.height < 3))
+    {
+        ROS_WARN("Map size is only x: %d,  y: %d . Not running map to image conversion", map->info.width, map->info.height);
+        return;
+    }
+    currentMap.header = map->header;
+    currentMap.info = map->info;
+    currentMap.data = map->data;
+
+    // resize cv image if it doesn't have the same dimensions as the map
+    if ((cv_img_full_.image.rows != map->info.height) && (cv_img_full_.image.cols != map->info.width))
+    {
+        cv_img_full_.image = cv::Mat(map->info.height, map->info.width, CV_8U);
+    }
+
+    //We have to flip around the y axis, y for image starts at the top and y for map at the bottom
+    int size_y_rev = cv_img_full_.image.rows - 1;
+    for (int y = size_y_rev; y >= 0; --y)
+    {
+        int idx_map_y = cv_img_full_.image.cols * y;
+        int idx_img_y = cv_img_full_.image.cols * (cv_img_full_.image.rows - y);
+
+        for (int x = 0; x < cv_img_full_.image.cols; ++x)
+        {
+            int idx = idx_img_y + x;
+            switch (currentMap.data[idx_map_y + x])
+            {
+            case -1:
+                cv_img_full_.image.data[idx] = 127;
+                break;
+            case 0:
+                cv_img_full_.image.data[idx] = 255;
+                break;
+            case 100:
+                cv_img_full_.image.data[idx] = 0;
+                break;
+            }
+        }
+    }
+    publishFullMap(true);
+}
+
+int8_t MapAsImageProvider::getCellOccupancy(float x, float y)
+{
+    int32_t grid_x = (x - (int)currentMap.info.origin.position.x) / currentMap.info.resolution;
+    int32_t grid_y = (y - (int)currentMap.info.origin.position.y) / currentMap.info.resolution;
+    if (grid_x >= 0 && grid_x < currentMap.info.width && grid_y >= 0 && grid_y < currentMap.info.height)
+    {
+        return currentMap.data[grid_y * currentMap.info.width + grid_x];
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+void MapAsImageProvider::tileUpdate()
+{
+
+    // check if pose_ptr_ is valid, otherwise, there is no starting point for tile
+    if (pose_ptr_)
+    {
+        int8_t currentPoint;
+        for (int x = 0; x < cv_img_tile_.image.cols; x++)
+        {
+            for (int y = 0; y < cv_img_tile_.image.rows; y++)
+            {
+                int idx = (cv_img_tile_.image.rows - y) * cv_img_tile_.image.cols + x;
+                switch (getCellOccupancy(
+                    pose_ptr_->pose.position.x + ((x - cv_img_tile_.image.cols / 2) * map_scale),
+                    pose_ptr_->pose.position.y + ((y - cv_img_tile_.image.rows / 2) * map_scale)))
+                {
+                case -1:
+                    cv_img_tile_.image.data[idx] = 127;
+                    break;
+                case 0:
+                    cv_img_tile_.image.data[idx] = 255;
+                    break;
+                case 100:
+                    cv_img_tile_.image.data[idx] = 0;
+                    break;
+                }
+            }
+        }
+    }
+}
--- src/map_to_img.cpp
+++ src/map_to_img.cpp
@@ -1,286 +1,31 @@
 #include "ros/ros.h"
-
-#include <nav_msgs/GetMap.h>
-#include <geometry_msgs/Quaternion.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <cv_bridge/cv_bridge.h>
-#include <image_transport/image_transport.h>
-#include <Eigen/Geometry>
-#include <sensor_msgs/image_encodings.h>
-#include <hector_map_tools/HectorMapTools.h>
 #include <std_msgs/Int16.h>
+#include <MapAsImageProvider.h>
 
 using namespace std;
 
-int map_scale = 50;
-/**
- * @brief This node provides occupancy grid maps as images via image_transport, so the transmission consumes less bandwidth.
- * The provided code is a incomplete proof of concept.
- */
-class MapAsImageProvider
+MapAsImageProvider *map_image_provider;
+
+void map_zoom_callback(const std_msgs::Int16 &scale)
 {
-public:
-cv::Mat *map_mat;
-int size_x = 256;
-int size_y = 256;
-
-std::vector<int8_t> map_data;
-
-nav_msgs::OccupancyGrid mapInstance;
-
-  MapAsImageProvider()
-    : pn_("~")
-  {
-
-    image_transport_ = new image_transport::ImageTransport(n_);
-    image_transport_publisher_full_ = image_transport_->advertise("map_image/full", 1);
-    image_transport_publisher_tile_ = image_transport_->advertise("map_image/tile", 1);
-
-    pose_sub_ = n_.subscribe("pose", 1, &MapAsImageProvider::poseUpdate, this);
-    map_sub_ = n_.subscribe("map", 1, &MapAsImageProvider::mapUpdate, this);
-
-    map_mat  = &cv_img_full_.image;
-    *map_mat = cv::Mat(256,256,CV_8U, 127);
-    map_mat  = &cv_img_tile_.image;
-    *map_mat = cv::Mat(256,256,CV_8U, 127);
-
-    //Which frame_id makes sense?
-    cv_img_full_.header.frame_id = "map_image";
-    cv_img_full_.encoding = sensor_msgs::image_encodings::MONO8;
-
-    cv_img_tile_.header.frame_id = "map_image";
-    cv_img_tile_.encoding = sensor_msgs::image_encodings::MONO8;
-
-    //Fixed cell width for tile based image, use dynamic_reconfigure for this later
-    p_size_tiled_map_image_x_ = 256;
-    p_size_tiled_map_image_y_ = 256;
-
-    ROS_INFO("Map to Image node started.");
-  }
-
-  ~MapAsImageProvider()
-  {
-    delete image_transport_;
-  }
-
-  void additionalPublisher()
-  { 
-    image_transport_publisher_full_.publish(cv_img_full_.toImageMsg());
-    image_transport_publisher_tile_.publish(cv_img_tile_.toImageMsg());
-  }
-
-  //We assume the robot position is available as a PoseStamped here (querying tf would be the more general option)
-  void poseUpdate(const geometry_msgs::PoseStampedConstPtr& pose)
-  { 
-    pose_ptr_ = pose;
-    publishMaps();
-  }
-
-  //The map->image conversion runs every time a new map is received at the moment
-  void mapUpdate(const nav_msgs::OccupancyGridConstPtr& map)
-  { 
-    mapInstance.header = map->header;
-    mapInstance.info = map->info;
-    mapInstance.data = map->data;
-    ROS_INFO("Received map");
-    size_x = map->info.width;
-    size_y = map->info.height;
-
-    if ((size_x < 3) || (size_y < 3) ){
-      ROS_WARN("Map size is only x: %d,  y: %d . Not running map to image conversion", size_x, size_y);
-      return;
-    }
-
-    // Only if someone is subscribed to it, do work and publish full map image
-      map_mat  = &cv_img_full_.image;
-
-      // resize cv image if it doesn't have the same dimensions as the map
-      if ( (map_mat->rows != size_y) && (map_mat->cols != size_x)){
-        *map_mat = cv::Mat(size_y, size_x, CV_8U);
-      }
-
-      map_data =  map->data;
-
-  }
-
-
-  void publishMaps(){
-      unsigned char *map_mat_data_p=(unsigned char*) map_mat->data;
-
-      //We have to flip around the y axis, y for image starts at the top and y for map at the bottom
-      int size_y_rev = size_y-1;
-
-      //for (int y = 0; y >= size_y_rev; ++y){
-
-      for (int y = size_y_rev; y >= 0; --y){
-
-        int idx_map_y = size_x * y;
-        int idx_img_y = size_x * (size_y -y);
-
-        for (int x = 0; x < size_x; ++x){
-
-          int idx = idx_img_y + x;
-
-          switch (map_data[idx_map_y + x])
-          {
-          case -1:
-            map_mat_data_p[idx] = 127;
-            break;
-
-          case 0:
-            map_mat_data_p[idx] = 255;
-            break;
-
-          case 100:
-            map_mat_data_p[idx] = 0;
-            break;
-          }
-        }
-    }
-
-    // Only if someone is subscribed to it, do work and publish tile-based map image Also check if pose_ptr_ is valid
-    if (pose_ptr_){
-
-      world_map_transformer_.setTransforms(mapInstance);
-
-      Eigen::Vector2f rob_position_world (pose_ptr_->pose.position.x, pose_ptr_->pose.position.y);
-      Eigen::Vector2f rob_position_map (world_map_transformer_.getC2Coords(rob_position_world));
-
-      Eigen::Vector2i rob_position_mapi (rob_position_map.cast<int>());
-
-      Eigen::Vector2i tile_size_lower_halfi (p_size_tiled_map_image_x_*map_scale/100, p_size_tiled_map_image_y_*map_scale/100);
-
-      Eigen::Vector2i min_coords_map (rob_position_mapi - tile_size_lower_halfi);
-
-      //Clamp to lower map coords
-      if (min_coords_map[0] < 0){
-        min_coords_map[0] = 0;
-      }
-
-      if (min_coords_map[1] < 0){
-        min_coords_map[1] = 0;
-      }
-
-      Eigen::Vector2i max_coords_map (min_coords_map + Eigen::Vector2i(p_size_tiled_map_image_x_*map_scale/50,p_size_tiled_map_image_y_*map_scale/50));
-
-      //Clamp to upper map coords
-      if (max_coords_map[0] > size_x){
-
-        int diff = max_coords_map[0] - size_x;
-        min_coords_map[0] -= diff;
-
-        max_coords_map[0] = size_x;
-      }
-
-      if (max_coords_map[1] > size_y){
-
-        int diff = max_coords_map[1] - size_y;
-        min_coords_map[1] -= diff;
-
-        max_coords_map[1] = size_y;
-      }
-
-      //Clamp lower again (in case the map is smaller than the selected visualization window)
-      if (min_coords_map[0] < 0){
-        min_coords_map[0] = 0;
-      }
-
-      if (min_coords_map[1] < 0){
-        min_coords_map[1] = 0;
-      }
-
-      Eigen::Vector2i actual_map_dimensions(max_coords_map - min_coords_map);
-
-      cv::Mat* map_mat  = &cv_img_tile_.image;
-
-      // resize cv image if it doesn't have the same dimensions as the selected visualization window
-      if ( (map_mat->rows != actual_map_dimensions[0]) || (map_mat->cols != actual_map_dimensions[1])){
-        *map_mat = cv::Mat(actual_map_dimensions[0], actual_map_dimensions[1], CV_8U);
-      }
-
-      const std::vector<int8_t>& map_data (mapInstance.data);
-
-      unsigned char *map_mat_data_p=(unsigned char*) map_mat->data;
-
-      //We have to flip around the y axis, y for image starts at the top and y for map at the bottom
-      int y_img = max_coords_map[1]-1;
-
-      for (int y = min_coords_map[1]; y < max_coords_map[1];++y){
-
-        int idx_map_y = y_img-- * size_x;
-        int idx_img_y = (y-min_coords_map[1]) * actual_map_dimensions.x();
-
-        for (int x = min_coords_map[0]; x < max_coords_map[0];++x){
-
-          int img_index = idx_img_y + (x-min_coords_map[0]);
-
-          switch (map_data[idx_map_y+x])
-          {
-          case 0:
-            map_mat_data_p[img_index] = 255;
-            break;
-
-          case -1:
-            map_mat_data_p[img_index] = 127;
-            break;
-
-          case 100:
-            map_mat_data_p[img_index] = 0;
-            break;
-          }
-        }        
-      }
-      image_transport_publisher_full_.publish(cv_img_full_.toImageMsg());
-      image_transport_publisher_tile_.publish(cv_img_tile_.toImageMsg());
-    }
-  }
-
-  ros::Subscriber map_sub_;
-  ros::Subscriber pose_sub_;
-
-  image_transport::Publisher image_transport_publisher_full_;
-  image_transport::Publisher image_transport_publisher_tile_;
-
-  image_transport::ImageTransport* image_transport_;
-
-  geometry_msgs::PoseStampedConstPtr pose_ptr_;
-
-  cv_bridge::CvImage cv_img_full_;
-  cv_bridge::CvImage cv_img_tile_;
-
-  ros::NodeHandle n_;
-  ros::NodeHandle pn_;
-
-  int p_size_tiled_map_image_x_;
-  int p_size_tiled_map_image_y_;
-
-  HectorMapTools::CoordinateTransformer<float> world_map_transformer_;
-
-};
-
-void task_callback(const std_msgs::Int16 &scale)
-{
-  map_scale = scale.data;
+  map_image_provider->setScale((float)scale.data / 2500);
 }
 
-int main(int argc, char** argv)
-{   
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "map_to_image_node");
-  ros::NodeHandle pn("~");
   ros::NodeHandle n("~");
-  //void task_callback();
-  ros::Rate loop_rate(50); 
-  ros::Subscriber map_zoom_sub = n.subscribe("/map_zoom", 1, task_callback);
+  ros::Rate loop_rate(50);
   ROS_INFO("Init MapAsImageProvider object");
-  MapAsImageProvider map_image_provider;
+  map_image_provider = new MapAsImageProvider(n);
+  ros::Subscriber map_zoom_sub = n.subscribe("/map_zoom", 1, map_zoom_callback);
 
   while (ros::ok())
-   { 
-      ros::spinOnce();
-      loop_rate.sleep();
-   }
+  {
+    ros::spinOnce();
+    loop_rate.sleep();
+    map_image_provider->publishFullMap();
+    map_image_provider->publishMapTile();
+  }
   return 0;
 }
