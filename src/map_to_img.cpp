a3fd8836f227b989fb67d6be527c490afba9f234
Łukasz Mitka <lukasz.mitka@husarion.com>
2019-07-17 15:00:10 +0200

    Add parameters for tile size

--- src/map_to_img.cpp
+++ src/map_to_img.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 MapAsImageProvider *map_image_provider;
+int32_t tile_width;
+int32_t tile_height;
 
 void map_zoom_callback(const std_msgs::Int16 &scale)
 {
@@ -15,9 +17,11 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "map_to_image_node");
   ros::NodeHandle n("~");
+  n.param<int32_t>("tile_width", tile_width, INITIAL_TILE_SIZE_X);
+  n.param<int32_t>("tile_height", tile_height, INITIAL_TILE_SIZE_Y);
   ros::Rate loop_rate(50);
   ROS_INFO("Init MapAsImageProvider object");
-  map_image_provider = new MapAsImageProvider(n);
+  map_image_provider = new MapAsImageProvider(n, tile_width, tile_height);
   ros::Subscriber map_zoom_sub = n.subscribe("/map_zoom", 1, map_zoom_callback);
 
   while (ros::ok())
